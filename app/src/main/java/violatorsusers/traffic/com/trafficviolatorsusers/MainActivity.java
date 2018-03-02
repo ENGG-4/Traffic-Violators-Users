e14edc29bb89f9b950440bf9679e89adaef68867
Jibin Philipose <philiposejibin@gmail.com>
2018-03-02 08:45:06 +0530

    Starting with a basic activity

---
+++ app/src/main/java/violatorsusers/traffic/com/trafficviolatorsusers/MainActivity.java
@@ -0,0 +1,13 @@
+package violatorsusers.traffic.com.trafficviolatorsusers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
