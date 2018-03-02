5fc32b51e8f241686c767f771f270c734e65dec7
Jibin Philipose <philiposejibin@gmail.com>
2018-03-02 11:31:13 +0530

    Added Google Sign-In authentication for users.

--- app/src/main/java/violatorsusers/traffic/com/trafficviolatorsusers/MainActivity.java
+++ app/src/main/java/violatorsusers/traffic/com/trafficviolatorsusers/MainActivity.java
@@ -3,6 +3,8 @@ package violatorsusers.traffic.com.trafficviolatorsusers;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseOptions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    // Manually configure Firebase Options
+    FirebaseOptions options = new FirebaseOptions.Builder()
+            .setApplicationId("1:523255386290:android:8e5eb46ead6e471f") // Required for Analytics.
+            .setApiKey("AIzaSyAXrZ2EoBWRh24BnQQUJUDpkvIZDENWCyk") // Required for Auth.
+            .setDatabaseUrl("https://trafficviolators-f0e4c.firebaseio.com/") // Required for RTDB.
+            .build();
 }
